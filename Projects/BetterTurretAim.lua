12ff064ad0fd2ad06286072a29b2f4c6bfb15006
HelyusHD <you@example.com>
2023-10-08 14:39:53 +0200

    project for smooth turret aim

---
+++ Projects/BetterTurretAim.lua
@@ -0,0 +1,15 @@
+TurretCodeName = "BT"
+
+
+
+function FindAllSubconstructs(I, TurretCodeName)
+    local ChosenSubconstructs = {}
+    local SubconstructsCount = I:GetAllSubconstructsCount()
+    for index = 0, SubconstructsCount do
+        local SubConstructIdentifier = I:GetSubConstructIdentifier(index)
+        if string.find(I:GetSubConstructInfo(SubConstructIdentifier).CustomName, CodeWord) then
+            table.insert(ChosenSubconstructs, SubConstructIdentifier)
+        end
+    end
+    return ChosenSubconstructs
+end
