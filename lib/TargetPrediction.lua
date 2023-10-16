ff40a3f9f5ac4ee3796d65f8fb537b12b06618d1
HelyusHD <you@example.com>
2023-10-16 03:53:30 +0200

    fixed g

--- lib/TargetPrediction.lua
+++ lib/TargetPrediction.lua
@@ -4,13 +4,12 @@
 -- Target               = TargetInfoObject containing {Position=Vector3, Velocity=Vector3, Acceleration=Vector3}
 -- Pos                  = Vector3 where the bullet spawns in global space
 -- Vel                  = scalar speed of the bullet
--- g                    = scalar gravitational acceleration
 -- Mass                 = bullet mass
 -- Drag                 = Drag of the bullet in [N*s/m] (which are newton per, meter per second)
 -- MaxIterationSteps    = maximum iterations to get more accurate
 -- Accuracy             = Accuracy in meters of the aproximation
 
-function TargetPrediction(I,Target,Pos,Vel,g,Mass,Drag,MaxIterationSteps,Accuracy)
+function TargetPrediction(I,Target,Pos,Vel,Mass,Drag,MaxIterationSteps,Accuracy)
     local Distance = (Target.Position - Pos).magnitude
     --I:Log("Distance: "..Distance)
     local PredictedPosition = Target.Position
@@ -23,7 +22,7 @@ function TargetPrediction(I,Target,Pos,Vel,g,Mass,Drag,MaxIterationSteps,Accurac
         PredictedPositionLast = PredictedPosition
         PredictedPosition = Target.Position + Target.Velocity * InterceptionTime + Target.Acceleration * InterceptionTime^2 / 2
         local Dy = PredictedPosition.y - Pos.y
-        Vy = Dy/InterceptionTime - g*InterceptionTime -- Dy = InterceptionTime * Vy + g*InterceptionTime^2
+        Vy = Dy/InterceptionTime - I:GetGravityForAltitude(Pos.y + Dy/2)*InterceptionTime -- Dy = InterceptionTime * Vy + g*InterceptionTime^2
         local Vx = math.sqrt(Vel^2 - Vy^2) -- Vel^2 = Vy^2 + Vx^2
         Distance = (PredictedPosition - Pos).magnitude
         InterceptionTime = Distance/(Vx - (Vx*Drag/Mass * InterceptionTime^2 / 2))
@@ -42,13 +41,12 @@ function Update(I)
     local Target = {Position=Vector3(1000,100,0),Velocity=Vector3(10,0,0),Acceleration=Vector3(12,-4,40)}
     local Pos = Vector3(0,0,0)
     local Vel = 800
-    local g = -10
     local Mass = 1
     local Drag = 0.01 -- if 100 m/s fast, experiences 1 N of force
     local MaxIterationSteps = 100
     local Accuracy = 1
-    
-    local result = TargetPrediction(I,Target,Pos,Vel,g,Mass,Drag,MaxIterationSteps,Accuracy)
+
+    local result = TargetPrediction(I,Target,Pos,Vel,Mass,Drag,MaxIterationSteps,Accuracy)
     if result.Valid then
         I:Log("InterceptionPoint: "..tostring(result.InterceptionPoint).."   InterceptionTime: "..tostring(result.InterceptionTime))
     else
