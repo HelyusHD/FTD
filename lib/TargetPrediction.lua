d92131e063f75b4fcb26a8c294a1606f91fc742e
HelyusHD <93012374+HelyusHD@users.noreply.github.com>
2023-10-16 03:47:43 +0200

    Update TargetPrediction.lua

--- lib/TargetPrediction.lua
+++ lib/TargetPrediction.lua
@@ -2,7 +2,7 @@
 -- for a gun fireing a bullet on a moving target.
 
 -- Target               = TargetInfoObject containing {Position=Vector3, Velocity=Vector3, Acceleration=Vector3}
--- Pos                  = where the bullet spawns in global space
+-- Pos                  = Vector3 where the bullet spawns in global space
 -- Vel                  = scalar speed of the bullet
 -- g                    = scalar gravitational acceleration
 -- Mass                 = bullet mass
