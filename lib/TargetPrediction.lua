965dc03f1cae0016c0286648de11a19888242d42
HelyusHD <you@example.com>
2023-10-16 04:03:48 +0200

    removed comments

--- lib/TargetPrediction.lua
+++ lib/TargetPrediction.lua
@@ -14,7 +14,6 @@
 
 function TargetPrediction(I,Target,Pos,Vel,Mass,Drag,MaxIterationSteps,Accuracy)
     local Distance = (Target.Position - Pos).magnitude
-    --I:Log("Distance: "..Distance)
     local PredictedPosition = Target.Position
     local InterceptionTime = Distance/Vel
     local PredictedPositionLast = Target.Position + Target.Position.normalized * (Accuracy+1)
@@ -25,8 +24,8 @@ function TargetPrediction(I,Target,Pos,Vel,Mass,Drag,MaxIterationSteps,Accuracy)
         PredictedPositionLast = PredictedPosition
         PredictedPosition = Target.Position + Target.Velocity * InterceptionTime + Target.Acceleration * InterceptionTime^2 / 2
         local Dy = PredictedPosition.y - Pos.y
-        Vy = Dy/InterceptionTime - I:GetGravityForAltitude(Pos.y + Dy/2).y*InterceptionTime -- Dy = InterceptionTime * Vy + g*InterceptionTime^2
-        local Vx = math.sqrt(Vel^2 - Vy^2) -- Vel^2 = Vy^2 + Vx^2
+        Vy = Dy/InterceptionTime - I:GetGravityForAltitude(Pos.y + Dy/2).y*InterceptionTime
+        local Vx = math.sqrt(Vel^2 - Vy^2)
         Distance = (PredictedPosition - Pos).magnitude
         InterceptionTime = Distance/(Vx - (Vx*Drag/Mass * InterceptionTime^2 / 2))
         I:Log("Iteration: "..Iterations.."   PredictedPosition: "..tostring(PredictedPosition).."   InterceptionTime: "..InterceptionTime.."   Vx: "..Vx)
